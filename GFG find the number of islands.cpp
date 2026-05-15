972a2d6f7a362e7b981771a8987e542a74ff3f20
Abhirampadavala <143874905+Abhirampadavala@users.noreply.github.com>
2026-05-15 14:35:48 +0530

    Update GFG find the number of islands.cpp

--- GFG find the number of islands.cpp
+++ GFG find the number of islands.cpp
@@ -2,8 +2,9 @@ class Solution {
   public:
   int rows[8]={-1,0,1,-1,1,-1,0,1};
   int cols[8]={1,1,1,0,0,-1,-1,-1};
-  
-  bool valid(int p,int q,int n,int m)
+
+//This is to check whether the neighbouring coordinates for the land coordinate are valid or not 
+ bool valid(int p,int q,int n,int m)
   {
       if(p>-1 && p<n && q>-1 && q<m)
       {
@@ -11,6 +12,12 @@ class Solution {
       }
       else return 0;
   }
+
+//In this DFS function you are sending the coordinates of that land 
+//piece found out 
+//then size of the grid, grid itself
+
+//Then using this nice method to check all the surrounding pieces of that land
   
   void DFS(int i,int j,int n,int m,vector<vector<char>>& grid)
   {
@@ -23,6 +30,13 @@ class Solution {
          }
       }
   }
+
+
+//This is the only thing given in question,so here I have initialized all necessary things,
+//then as usual for loop which will be of O(n*m) TC  as it was mentioned in expected complexities
+//so if you find a land then DFS there, i.e send that coordinate (i,j) to DFS function
+//The DFS function connects all land pieces attached to each other
+//Now everytime you find an 'L',do count++ then you get to do a DFS completing that one island. 
     int countIslands(vector<vector<char>>& grid) {
         // Code here
         int n=grid.size();
