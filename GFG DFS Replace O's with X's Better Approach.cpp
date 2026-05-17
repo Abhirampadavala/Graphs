f89281f4a1981d333a592b2ecb4c8e436cdae30f
Abhirampadavala <143874905+Abhirampadavala@users.noreply.github.com>
2026-05-17 16:25:32 +0530

    Update GFG DFS Replace O's with X's Better Approach.cpp

--- GFG DFS Replace O's with X's Better Approach.cpp
+++ GFG DFS Replace O's with X's Better Approach.cpp
@@ -1,1 +1,4 @@
-
+//In this approach , first we will traverse along the boundary of the 2D matrix
+//then if we find any block grid[i][j]=='O' then do a graph traversal on it and change all the components of graph to 'V'
+//Now all the boundary 'O's are eliminated and insider 'O's are left
+//Now do a double for loop on 2D matrix and change all the 'O's to 'X's and all the 'V's to 'O's
